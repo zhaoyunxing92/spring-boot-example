d4874382ecc94e218d5401bfb8773f5010103c43
zhaoyunxing92 <2385585770@qq.com>
2018-07-18 21:23:03 +0800

    添加：hello example

---
+++ boot-example-hello/src/main/java/com/sunny/example/hello/BootExampleHelloApplication.java
@@ -0,0 +1,12 @@
+package com.sunny.example.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootExampleHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootExampleHelloApplication.class, args);
+    }
+}
---
+++ boot-example-hello/src/main/java/com/sunny/example/hello/controller/HelloController.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright(C) 2018 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package com.sunny.example.hello.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhaoyunxing
+ * @class: com.sunny.example.hello.controller.HelloController
+ * @date: 2018-07-18 21:06
+ * @des:
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+}
